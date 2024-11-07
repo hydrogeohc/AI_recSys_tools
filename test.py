8a8fd920fed118ff8b1b432195e68b8db850e43c
hydroml <hydrogeo@hydromls-MacBook-Air.local>
2024-11-06 20:34:27 -0800

    ok

--- test.py
+++ test.py
@@ -4,4 +4,5 @@ for i in range(len(data)):
     data[i]['sales_lag_1'] = data[i - 1]['sales'] if i >= 1 else None
     data[i]['sales_lag_3'] = data[i - 3]['sales'] if i >= 3 else None
 
-print(data
+
+print(data)
